73b4d06d940b4679ea3c61b770caf3550ed68567
Tom Fattah <thomas.fattah@gmail.com>
2020-04-07 16:47:09 -0400

    Added ZipCode to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import ZipCode from './ZipCode';
+
 function Home() {
   return (
     <div>
+      <ZipCode />
       <h3>Why Roasted On?</h3>
       <h4>We believe in the freshest coffee</h4>
       <p>

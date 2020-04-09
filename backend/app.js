a21193eadae0cf038f227dedc543a9e44d770ddb
Tom Fattah <thomas.fattah@gmail.com>
2020-04-09 12:42:51 -0400

    Added body parser

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const connection = require('dbcon.js');
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'dbuser',
-  password: 's3kreee7',
-  database: 'my_db',
-});
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+connection.connect(function (err) {
   if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+  console.log('Connected!');
 });
 
-connection.end();
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/search', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)

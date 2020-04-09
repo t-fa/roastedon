f668ad2d2492a184f565921c808700e64318a9dd
Tom Fattah <thomas.fattah@gmail.com>
2020-04-09 13:07:51 -0400

    Created started SQL queries

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,39 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const connection = require('dbcon.js');
-const bodyParser = require('body-parser');
+const express = require('express'),
+  app = express(),
+  port = 3000,
+  connection = require('./dbcon.js'),
+  bodyParser = require('body-parser');
 
 connection.connect(function (err) {
   if (err) throw err;
   console.log('Connected!');
 });
 
-app.get('/search', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  const context = {};
+  connection.query('SELECT * FROM users', function (err, rows) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.send(context.results);
+  });
+});
+
+app.get('/create', (req, res) => {
+  const sql = `
+  CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+  )`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log('Table created!');
+  });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)

e548b8ac1152e9a4cd9cd5ead2040906ac3390c7
Tom Fattah <thomas.fattah@gmail.com>
2020-04-17 13:49:12 -0400

    Changed to functional component

--- frontend/src/components/MainLayout.js
+++ frontend/src/components/MainLayout.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Home from './Home';
 import AddShopForm from './AddShopForm';
-import ShopThumbnailView from './ShopThumbnailView';
 
-class MainLayout extends Component {
-  state = {
-    shops: [],
-  };
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/add" component={AddShopForm} />
-          <Route path="/shops" component={ShopThumbnailView} />
-        </Switch>
-      </div>
-    );
-  }
-}
+const mainLayout = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/add" component={AddShopForm} />
+      </Switch>
+    </div>
+  );
+};
 
-export default MainLayout;
+export default mainLayout;

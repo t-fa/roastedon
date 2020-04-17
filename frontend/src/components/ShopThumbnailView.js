9548956a0441ad03dcf7c1cb50025a4346234119
Tom Fattah <thomas.fattah@gmail.com>
2020-04-17 13:55:32 -0400

    Displays all info from DB

--- frontend/src/components/ShopThumbnailView.js
+++ frontend/src/components/ShopThumbnailView.js
@@ -3,17 +3,14 @@ import React from 'react';
 const shopThumbnailView = (props) => {
   return (
     <div className="shopthumbnailview container border rounded-lg p-3">
-      {/* <img class="shopthumbnailimg" src={props.imgsrc} /> */}
-      <h4>{props.shopName}</h4>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <h4>{props.name}</h4>
+      <ul>
+        <li>Address: {props.address1}</li>
+        <li>Apt: {props.address2}</li>
+        <li>Zip: {props.zipcode}</li>
+        <li>City: {props.city}</li>
+        <li>State: {props.state}</li>
+      </ul>
     </div>
   );
 };

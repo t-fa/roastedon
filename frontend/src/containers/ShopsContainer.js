0751f6383f650b218746f5124a55b96f50cf4f0c
Tom Fattah <thomas.fattah@gmail.com>
2020-06-29 17:02:33 -0400

    Converted ShopsContainer to use hooks

--- frontend/src/containers/ShopsContainer.js
+++ frontend/src/containers/ShopsContainer.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import * as actionTypes from '../store/actions/shops';
 
 import Shops from '../components/Shops/Shops';
 
-class ShopsContainer extends Component {
-  componentDidMount() {
-    this.props.onAddedShops(this.props.zipcode);
-    console.log(`Zip: ${this.props.zipcode}`);
-    console.log(`Shops: ${this.props.shops}`);
-  }
+const ShopsContainer = (props) => {
+  useEffect(() => {
+    props.onAddedShops(props.zipcode);
+  }, [props.zipcode]);
 
-  render() {
-    return <Shops shops={this.props.shops} />;
-  }
-}
+  return <Shops shops={props.shops} />;
+};
 
 const mapStateToProps = (state) => {
   return {

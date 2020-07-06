800f652027b1addca868757c7f6db2cd3738443f
Tom Fattah <thomas.fattah@gmail.com>
2020-07-06 12:01:37 -0400

    Added useEffect dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,11 @@ import * as actions from './store/actions/auth';
 import MainLayout from './containers/MainLayout';
 
 const App = (props) => {
+  const { onTryAutoSignup } = props;
+
   useEffect(() => {
-    props.onTryAutoSignup();
-  }, [props.onTryAutoSignup]);
+    onTryAutoSignup();
+  }, [onTryAutoSignup]);
 
   return (
     <BrowserRouter>

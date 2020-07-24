019d23040d5d07509604d5f4e0543dfa59f0cf71
Tom Fattah <thomas.fattah@gmail.com>
2020-07-24 11:27:36 -0400

    Added margin

--- frontend/src/components/Home/HomeLayout.js
+++ frontend/src/components/Home/HomeLayout.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import SearchBar from './SearchBar';
 import Info from './Info';
 
+const Div = styled.div`
+  margin-top: 100px;
+`;
+
 const homeLayout = (props) => {
   return (
-    <>
+    <Div>
       <SearchBar
         zipcode={props.zipcode}
         onChange={props.onChange}
         handleSubmit={props.handleSubmit}
       />
       <Info />
-    </>
+    </Div>
   );
 };
 

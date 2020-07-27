2bf6d2baf64bf2db5f38551a2b4ee0bb7bb45885
Tom Fattah <thomas.fattah@gmail.com>
2020-07-27 14:10:15 -0400

    Updated info text

--- frontend/src/components/Home/Info.js
+++ frontend/src/components/Home/Info.js
@@ -6,18 +6,22 @@ const info = () => {
       <h3>Why Roasted On?</h3>
       <h4>We believe in the freshest coffee</h4>
       <p>
-        Ordinary grocery store coffee comes with a "Best By" date, which we
-        consider an arbitrary date. Often the "Best By" date is months from when
-        the coffee is roasted. Many coffee enthusiasts would consider that
-        coffee to have gone bad. While the beans aren't expired in the
-        traditional sense, they're past the point where they offer the freshest
-        possible flavor. Roasters who us a "Roasted On" date believe in fresh
-        coffee. A "Roasted On" date tells you exactly the day the coffee was
-        roasted and allows you to enjoy the best possible cup.
+        Ordinary grocery store coffee comes with a "Best By" date, which is
+        usually an arbitrary date. Often the "Best By" date is months after the
+        coffee is roasted. Many coffee enthusiasts would consider such coffee to
+        have gone bad. While the beans aren't expired in the traditional sense,
+        they're past the point where they offer the freshest possible flavor.
+        Roasters who use a "Roasted On" date believe in fresh coffee. A "Roasted
+        On" date tells you exactly the day the coffee was roasted and allows you
+        to enjoy the best possible cup.
       </p>
 
-      <h3>More info</h3>
-      <p>Lorem ipsum dolor</p>
+      <h4>Do I need to be a homebrewer to use this site?</h4>
+      <p>
+        No! Coffee shops that sell coffee with a "Roasted On" date believe in
+        high quality coffee. If you're just stopping in for a coffee, you're
+        likely to enjoy a great cup.
+      </p>
     </section>
   );
 };
